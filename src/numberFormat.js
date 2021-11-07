23ae52d472632ac3d9513a6b9ac6d1fe351decca
Aldo Rifki Putra <aldoetobex@gmail.com>
2021-11-07 18:03:51 +0700

    Update

--- src/numberFormat.js
+++ src/numberFormat.js
@@ -1,10 +1,10 @@
 import currency from 'currency.js'
 
 export default function numberFormat(angka, prefix = '') {
-  return currency(angka, {
+  return currency(Number(angka), {
     symbol: prefix + ' ',
     separator: '.',
     decimal: ',',
-    precision: angka % 1 != 0 ? 2 : 0
+    precision: Number(angka) % 1 != 0 ? 2 : 0
   }).format()
 }
